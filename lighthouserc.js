19379c81d386e3973cd2255e6151f33d04f71db7
Daphne-Herve <daphne.herve@gmail.com>
2021-12-05 21:31:51 +0100

    ci: add server ready pattern & start server command

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,9 @@ module.exports = {
       startServerCommand: "npm start",
       url: ["http://localhost:4200/"],
       startServerReadyPattern: "Server is running on PORT 4000",
+      settings: {
+        onlyCategories: ["accessibility"],
+      },
     },
     upload: {
       target: "temporary-public-storage",
